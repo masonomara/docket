948e8ad69f27cc2ab146a2486d0ab7f93604520a
masonomara <mason.omara@gmail.com>
2025-12-21 08:26:40 -0500

    removed vibecoded stuff

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -8,5 +8,3 @@ export { buildAuthPage } from "./auth";
 export { buildOrgMembershipPage } from "./org-membership";
 export { buildOrgDeletionPage } from "./org-deletion";
 export { buildKBPage } from "./kb";
-export { buildTenantDOPage } from "./tenant-do";
-export { buildLLMPage } from "./llm";
